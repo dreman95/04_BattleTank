b07cb962fb4d302e76e58f681d9ba85b58a2dc85
Andre-PC\Andre <andreagius@outlook.com>
2018-09-09 21:25:41 +0100

    Made player controller posess PlayerTank

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,14 +2,26 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	UE_LOG(LogTemp, Warning, TEXT("Player controller begin play"));
+	
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Player controller not possessing tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Player controller possessing: %s"), *(ControlledTank->GetName()));
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const {
-
+	
 	//Get pawn from the tanks player controller perspective 
 	//Returns the pawn the playercontroller class is currently possesing 
 	return Cast<ATank>(GetPawn());
 
-	
-
 }
+
 
